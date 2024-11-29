76d90e232ecb618029a95bd184dadd84c9fe6b1e
sudhakant <sudhakantpanda5@gmail.com>
2024-11-29 17:42:02 +0530

    Getting Reliance Stock Price

--- test.js
+++ test.js
@@ -1,29 +1,11 @@
 'use strict';
+var request = require('request');
 
 require('dotenv').config();
 
-var request = require('request');
-
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-// var url = `https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&symbol=IBM&apikey=${process.env.API_KEY}`;
-
-// request.get({
-//     url: url,
-//     json: true,
-//     headers: {'User-Agent': 'request'}
-//   }, (err, res, data) => {
-//     if (err) {
-//       console.log('Error:', err);
-//     } else if (res.statusCode !== 200) {
-//       console.log('Status:', res.statusCode);
-//     } else {
-//       // data is successfully parsed as a JSON object:
-//       console.log(data);
-//     }
-// });
-
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = `https://www.alphavantage.co/query?function=ATR&symbol=IBM&interval=daily&time_period=14&apikey=${process.env.API_KEY}`;
+// var url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=reliance&apikey=${process.env.API_KEY}`;
+var url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=BAJAJFINSV.BSE&apikey=${process.env.API_KEY}`;
 
 request.get({
     url: url,
